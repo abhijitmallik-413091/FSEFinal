4f8eefae58c4a9468232a49016c5c744c40cccb5
abhijitmallik-413091 <54707332+abhijitmallik-413091@users.noreply.github.com>
2019-10-01 12:23:06 -0700

    FSE Final assignment

--- TaskManager/ProjectManager/DAC/TaskDBModel.Designer.cs
+++ TaskManager/ProjectManager/DAC/TaskDBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Administrator\Desktop\TaskManager\ProjectManager\DAC\TaskDBModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Administrator\Desktop\FSE_ASSIGNMENTS\FSE\FinalAssignment\TaskManager\ProjectManager\DAC\TaskDBModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
